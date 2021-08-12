30a30b6bdac3c05da26985d22f211cbe9836362f
Lovecraftian Horror <LovecraftianHorror@pm.me>
2021-08-11 22:23:25 -0400

    Add basic snapshot test

--- tests/rand_dir.rs
+++ tests/rand_dir.rs
@@ -1,5 +1,26 @@
 use rand_dir::{Dir, File, RandDir};
 
+use std::path::Path;
+
+use dir_diff::is_different;
+
+// Snapshot test that compares the directory to a correct one stored under `tests/assets`
+#[test]
+fn it_works() {
+    let ideal_dir = Path::new("tests")
+        .join("assets")
+        .join("it_works_ideal")
+        .join("base");
+
+    let rand_dir = RandDir::builder()
+        .dir(Dir::real().name("Outer dir"))
+        .file(File::zeroed().name("Outer file").size(128))
+        .try_build()
+        .unwrap();
+
+    assert!(!is_different(rand_dir.at(), ideal_dir).unwrap());
+}
+
 #[test]
 fn error_with_duplicate_entry_name() {
     let result = RandDir::builder()
